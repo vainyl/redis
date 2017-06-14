33f774182484c4d0d7a14d93189aa4146d859c07
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 17:12:00 +0300

    Added secure https

--- src/Extension/RedisExtension.php
+++ src/Extension/RedisExtension.php
@@ -21,4 +21,11 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class RedisExtension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function getCompilerPasses(): array
+    {
+        return [];
+    }
 }
