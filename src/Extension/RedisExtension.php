f39bff9ce3649eb271665be255d7169e08e41d5d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-16 10:25:48 +0300

    Register compiler pass

--- src/Extension/RedisExtension.php
+++ src/Extension/RedisExtension.php
@@ -21,4 +21,11 @@ use Vainyl\Core\Extension\AbstractFrameworkExtension;
  */
 class RedisExtension extends AbstractFrameworkExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function getCompilerPasses(): array
+    {
+     return [[new RedisScriptCompilerPass()]];
+    }
 }
